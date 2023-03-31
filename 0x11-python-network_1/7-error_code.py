640b01f2024aba5a4a89cebe3a1691160e334217
Victor Amonde <victor.amonde@gmail.com>
2023-03-31 05:33:39 -0700

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""script that takes in a URL, sends a request to the URL and displays the body of the response"""
+"""
+Use requests package to make a get request to given URL and display
+the body of response, or error code if error.
+"""
 import sys
 import requests
 
-
 if __name__ == "__main__":
     url = sys.argv[1]
-
     r = requests.get(url)
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
-    else:
+    try:
+        r.raise_for_status()
         print(r.text)
+    except Exception as e:
+        print("Error code: {}".format(r.status_code))
